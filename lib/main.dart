b56d9eec6da4f593f401a07ba541c5c87a6ef80c
Ibrahim0096891 <ibrahim0096891@gmail.com>
2024-07-09 01:58:02 +0600

    lives

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,88 @@
+
+// Current SDK version: 3.22.1
 import 'package:flutter/material.dart';
 
+// image, materialApp, scaffold, Icons,
+// buttons(ElevatedButton, IconButton, TextButton, GestureDetector, InkWell)
+
 void main() {
-  runApp(introApp());
+  runApp(IntroApp());
 }
 
-class introApp extends StatelessWidget {
+class IntroApp extends StatelessWidget {
+  const IntroApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(
-              backgroundColor: Colors.blueAccent,
-              title: Text('Home'),
-            ),
-            body: Center(
-              child: Text(
-                'Hello World. We are coming Bangladesh',
-                textAlign: TextAlign.start,
-                maxLines: 1,
-                style: TextStyle(
-                  fontSize: 32,
-                  fontWeight: FontWeight.w700,
-                  decoration: TextDecoration.underline,
-                  letterSpacing: 1,
-                  wordSpacing: 11,
-                  overflow: TextOverflow.fade,
-                ),
-              ),
-            )));
+      debugShowCheckedModeBanner: true,
+      home: Home(),
+      title: 'Intro app',
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.green,
+      appBar: AppBar(
+        title: Text('Home'),
+        backgroundColor: Colors.blue,
+      ),
+      body: Center(
+        // child: Icon(
+        //   Icons.tablet_android,
+        //   size: 50,
+        //   color: Colors.white,
+        // ),
+        // child: Image.network('https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Flag_of_Bangladesh.svg/2000px-Flag_of_Bangladesh.svg.png'),
+        // child: Image.asset(
+        //   'assets/images/messi.jpg',
+        //   width: 250,
+        //   height: 200,
+        //   fit: BoxFit.cover,
+        // ),
+        // child: ElevatedButton(
+        //   onPressed: () {
+        //     print('Button pressed');
+        //   },
+        //   child: Text('Notification'),
+        // ),
+        // child: IconButton(
+        //   onPressed: () {
+        //     print('Add');
+        //   },
+        //   icon: Icon(Icons.add),
+        // ),
+        // child: TextButton(
+        //   onPressed: () {
+        //     print('show button pressed');
+        //   },
+        //   child: Text('Text button'),
+        // ),
+        // child: GestureDetector(
+        //   onTap: () {
+        //     print('Single tap');
+        //   },
+        //   onDoubleTap: () {
+        //     print('Double tap');
+        //   },
+        //   child: Text('normal text'),
+        // ),
+        child: InkWell(
+          onTap: () {
+            print('Single tap');
+          },
+          onDoubleTap: () {
+            print('Double tap');
+          },
+          child: Text('normal text'),
+        ),
+      ),
+    );
   }
 }
