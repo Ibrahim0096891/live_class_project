665a5aa07584d82ac3d1672b9c89a45b771ee098
Ibrahim0096891 <ibrahim0096891@gmail.com>
2024-07-11 00:39:57 +0600

    push

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-
-
 // Current SDK version: 3.22.1
 import 'package:flutter/material.dart';
 
@@ -29,20 +27,169 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.green,
-      appBar: AppBar(
-        title: Text('Home'),
-        backgroundColor: Colors.blue,
-      ),
-      body: Center(
-        child: InkWell(
-          onTap: () {
-            print('Single tap');
-          },
-          onDoubleTap: () {
-            print('Double tap');
-          },
-          child: Text('normal text'),
+        backgroundColor: Colors.green,
+        appBar: AppBar(
+          title: Text('Home'),
+          backgroundColor: Colors.blue,
+        ),
+       /* body: Column(
+          // mainAxisAlignment: ,
+          // crossAxisAlignment: ,
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Text('Md Ibrahim Prodania........'),
+            Text('Ezaz Ahamed Fahim'),
+            Text('Abu Naser Toha'),
+            Text('Easin Hossen Arif'),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text('Click Here'),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.access_alarm),
+            ),
+          ],
+        )*/
+      /*body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Text('Hello World\n from ostad'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+            Text('Hello World'),
+          ],
+        ),
+      ),*/
+      body: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Row(
+          children: [
+            Column(
+              children: [
+                Text('Column 1')
+              ],
+            ),
+            Column(
+              children: [
+                Text('Column 2'),
+                Row(
+                  children: [
+                    Text('Ostad App Development'),
+                    Text('Ostad App Development'),
+                    Text('Ostad App Development'),
+                    Text('Ostad App Development'),
+                    Text('Ostad App Development'),
+                    Text('Ostad App Development'),
+                    Text('Ostad App Development'),
+                    Text('Ostad App Development'),
+                    Text('Ostad App Development'),
+                    Text('Ostad App Development'),
+                    Text('Ostad App Development'),
+                    Text('Ostad App Development'),
+                    Text('Ostad App Development'),
+                    Text('Ostad App Development'),
+                    Text('Ostad App Development'),
+                  ],
+                )
+              ],
+            ),
+            Column(
+              children: [
+                Text('Column 3')
+              ],
+            )
+          ],
         ),
       ),
     );
