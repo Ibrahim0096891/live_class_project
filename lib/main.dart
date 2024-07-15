5373b7069f562cbecf5931ddac03a3cef18ff57b
Md. Ibrahim Prodania <ibrahim0096891@gmail.com>
2024-07-16 01:20:25 +0600

    Container, dialog, bottom-sheet

--- lib/main.dart
+++ lib/main.dart
@@ -1,58 +1,185 @@
+// Current SDK version: 3.22.1
 import 'package:flutter/material.dart';
 
+// Container, dialog, bottom-sheet
+
 void main() {
-  runApp(introApp());
+  runApp(IntroApp());
 }
 
-class introApp extends StatelessWidget {
+class IntroApp extends StatelessWidget {
+  const IntroApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(
-              backgroundColor: Colors.blueAccent,
-              title: Text('Home'),
-            ),
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  width: 100,
-                  height: 100,
-                  // color: Colors.green,
-                  // padding: EdgeInsets.all(10),
-                  margin: EdgeInsets.all(16),
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                      color: Colors.green,
-                    border: Border.all(color: Colors.red, width: 4),
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(16),
-                      bottomRight: Radius.circular(16),
-                    ),
-                  ),
-                  child: Text('Sample'),
+      debugShowCheckedModeBanner: true,
+      home: Home(),
+      title: 'Intro app',
+    );
+  }
+}
 
-                ),
-                Container(
-                  width: 100,
-                  height: 100,
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  List<String> friendList = ['Fahad', 'Iram', 'Touhid', 'Sakib', 'Niloy'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text('Home'),
+        backgroundColor: Colors.blue,
+      ),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 100,
+              height: 100,
+              // color: Colors.green,
+              // padding: EdgeInsets.all(8),
+              // padding: EdgeInsets.symmetric(
+              //   vertical: 8,
+              //   horizontal: 4
+              // ),
+              padding: EdgeInsets.only(bottom: 8),
+              margin: EdgeInsets.all(16),
+              alignment: Alignment.bottomRight,
+              decoration: BoxDecoration(
                   color: Colors.green,
-                  padding: EdgeInsets.all(10),
-                  margin: EdgeInsets.all(8),
-
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(25.0),
-                  child: Text('Fuck You'),
-                ),
-              ],
+                  border: Border.all(color: Colors.red, width: 4),
+                  // borderRadius: BorderRadius.circular(16)
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(16),
+                      bottomRight: Radius.circular(16)),
+                  shape: BoxShape.rectangle,
+                  image: DecorationImage(
+                      image: AssetImage('assets/image/messi.jpg'),
+                      fit: BoxFit.cover,
+                      opacity: 0.4),
+                  boxShadow: <BoxShadow>[
+                    BoxShadow(
+                      color: Colors.red.withOpacity(0.5),
+                      spreadRadius: 3,
+                      blurRadius: 8,
+                      offset: Offset(0, 3),
+                    ),
+                    BoxShadow(
+                      color: Colors.green.withOpacity(0.2),
+                      spreadRadius: 3,
+                      blurRadius: 8,
+                      offset: Offset(0, 6),
+                    ),
+                  ]),
+              child: Text('Messi'),
             ),
-          ),
-
-        )
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.green,
+              padding: EdgeInsets.all(16),
+              margin: EdgeInsets.all(8),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(24.0),
+              child: Text('sdfsdf'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // showAboutDialog(
+                //   context: context,
+                //   applicationName: 'Sample',
+                //   applicationVersion: '1.0.1',
+                //   children: [
+                //     Text('Sample text')
+                //   ]
+                // );
+                showDialog(
+                    context: context,
+                    barrierDismissible: false,
+                    barrierColor: Colors.black26,
+                    builder: (ctx) {
+                      return AlertDialog(
+                        title: Text('Our custom dialog'),
+                        content: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('Sample text'),
+                            Text('Sample text'),
+                            Text('Sample text'),
+                          ],
+                        ),
+                        actions: [
+                          TextButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            child: Text('Cancel'),
+                          ),
+                          TextButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            child: Text('Okay'),
+                          )
+                        ],
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.zero,
+                        ),
+                        backgroundColor: Colors.white,
+                      );
+                    });
+              },
+              child: Text('Dialog'),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                showModalBottomSheet(
+                    backgroundColor: Colors.white,
+                    barrierColor: Colors.black54,
+                    enableDrag: false,
+                    isDismissible: false,
+                    context: context,
+                    builder: (ctx) {
+                      return SizedBox(
+                        height: 600,
+                        width: double.infinity,
+                        child: Column(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(20.0),
+                              child: Text('Title'),
+                            ),
+                            Divider(),
+                            Column(
+                              children: [
+                                Text('No-1'),
+                                Text('No-2'),
+                                Text('No-3'),
+                              ],
+                            )
+                          ],
+                        ),
+                      );
+                    });
+              },
+              child: Text('Show Bottom Sheet'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
